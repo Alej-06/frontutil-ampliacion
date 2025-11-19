773e4a7189af08ea96d1aaba242b53866d60594c
Rafael Aznar <rafaaznar@gmail.com>
2025-11-19 20:00:24 +0100

    feat: implement routed view for blog details with data fetching

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
 import { PlistBlog } from './component/blog/plist/plist';
-import { ViewBlog } from './component/blog/view/view';
+import { RoutedView } from './component/blog/routed-view/routed-view';
 
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'blog', component: PlistBlog },
-  { path: 'blog/view/:id', component: ViewBlog },
+  { path: 'blog/view/:id', component: RoutedView },
 ];
--- src/app/component/blog/view/view.ts
+++ src/app/component/blog/view/view.ts
@@ -1,8 +1,5 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
 import { IBlog } from '../../../model/blog';
-import { BlogService } from '../../../service/blog';
 
 @Component({
   selector: 'app-view',
@@ -11,29 +8,6 @@ import { BlogService } from '../../../service/blog';
   styleUrl: './view.css',
 })
 export class ViewBlog {
-  oBlog: IBlog | null = null;
+  @Input() oBlog: IBlog | null = null;
 
-  constructor(private oBlogService: BlogService, private route: ActivatedRoute) {
-    // Obtener el ID del blog desde la ruta
-    const idParam = this.route.snapshot.paramMap.get('id');
-    const blogId = idParam ? Number(idParam) : NaN;
-    if (isNaN(blogId)) {
-      console.error('Invalid blog id:', idParam);
-      return;
-    }
-    this.getBlog(blogId);
-  }
-
-  ngOnInit() { }
-
-  getBlog(blogId: number) {
-    this.oBlogService.get(blogId).subscribe({
-      next: (data: IBlog) => {
-        this.oBlog = data;
-      },
-      error: (error: HttpErrorResponse) => {
-        console.error('Error fetching blog:', error);
-      },
-    });
-  }
 }
